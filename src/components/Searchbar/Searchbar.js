3d2076ce14a29d813828f3acb7229c063bf4bdeb
MykhailoTarasov <123101959+MykhailoTarasov@users.noreply.github.com>
2023-12-12 11:14:22 +0200

    trying to fix

--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -1,23 +1,26 @@
-const SearchBar = ({onSubmit}) => {
-    return (
-        <header>
-        <form onSubmit={onSubmit}>
-          <button type="submit">
-            <span>Search</span>
-          </button>
-      
-          <input
-            class="input"
-            type="text"
-            autocomplete="off"
-            autofocus
-            placeholder="Search images and photos"
-          />
-        </form>
-      </header>
-    )
-}
+const SearchBar = ({ onSubmit }) => {
+  return (
+    <header>
+      <form
+        onSubmit={evt => {
+          evt.preventDefault();
+          const { searchQuery } = evt.target.elements;
+          onSubmit(searchQuery.value);
+          evt.target.reset();
+        }}
+      >
+        <button type="submit">
+          <span>Search</span>
+        </button>
+
+        <input
+          name="searchQuery"
+          type="text"
+          placeholder="Search images and photos"
+        />
+      </form>
+    </header>
+  );
+};
 
 export default SearchBar;
-
-
